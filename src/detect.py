8075ac705a760801854907b359b6c94bea2664f7
allegroai-git <>
2024-04-04 19:46:51 +0300

    Initial commit

---
+++ src/detect.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 12:47:01 2019
+
+@author: Keshik
+
+Source
+    https://github.com/packyan/PyTorch-YOLOv3-kitti
+"""
+
+from __future__ import division
+
+import datetime
+import os
+import random
+import time
+
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from PIL import Image
+from matplotlib.ticker import NullLocator
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from allegroai import Task, DataView, DatasetVersion
+
+# hack: fix root repository path
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.dataset_allegro import AllegroDataset
+from src.utils import non_max_suppression, load_classes
+from src.dataset import ImageFolder
+from src.model import Darknet
+
+
+# max labels is 48
+_n_cls_preds = 48
+_cmap = plt.get_cmap('tab10')
+_colors = [_cmap(i) for i in np.linspace(0, 1, _n_cls_preds)]
+_bbox_colors = random.sample(_colors, _n_cls_preds)
+
+
+def draw_boxes(path, detections, output_path=".", img_i=0, classes=None):
+    # TODO - remove this ugly hack
+    if not classes:
+        # default objects for YOLO 
+        base_objects = ["Car", "Van", "Truck", "Pedestrian", "Person_sitting", "Cyclist", "Tram", "Misc"]
+        classes = base_objects + [str(i) for i in range(len(base_objects), _n_cls_preds)]
+
+    # Create plot
+    img = np.array(Image.open(path))
+    plt.figure()
+    fig, ax = plt.subplots(1)
+    ax.imshow(img)
+
+    kitti_img_size = 416
+
+    # The amount of padding that was added
+    pad_x = max(img.shape[0] - img.shape[1], 0) * (kitti_img_size / max(img.shape))
+    pad_y = max(img.shape[1] - img.shape[0], 0) * (kitti_img_size / max(img.shape))
+    # Image height and width after padding is removed
+    unpad_h = kitti_img_size - pad_y
+    unpad_w = kitti_img_size - pad_x
+
+    # Draw bounding boxes and labels of detections
+    if detections is not None:
+        # print(type(detections))
+        # print(detections.size())
+        unique_labels = detections[:, -1].cpu().unique()
+        # n_cls_preds = len(unique_labels)
+        # bbox_colors = random.sample(colors, n_cls_preds)
+        for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
+
+            print('\t+ Label: %s, Conf: %.5f' % (classes[int(cls_pred)], cls_conf.item()))
+            # Rescale coordinates to original dimensions
+            box_h = int(((y2 - y1) / unpad_h) * (img.shape[0]))
+            box_w = int(((x2 - x1) / unpad_w) * (img.shape[1]))
+            y1 = int(((y1 - pad_y // 2) / unpad_h) * (img.shape[0]))
+            x1 = int(((x1 - pad_x // 2) / unpad_w) * (img.shape[1]))
+
+            color = _bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
+            # Create a Rectangle patch
+            bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2,
+                                     edgecolor=color,
+                                     facecolor='none')
+            # Add the bbox to the plot
+            ax.add_patch(bbox)
+            # Add label
+            plt.text(x1, y1 - 30, s=classes[int(cls_pred)] + ' ' + str('%.4f' % cls_conf.item()), color='white',
+                     verticalalignment='top',
+                     bbox={'color': color, 'pad': 0})
+            
+    # Save generated image with detections
+    plt.axis('off')
+    plt.gca().xaxis.set_major_locator(NullLocator())
+    plt.gca().yaxis.set_major_locator(NullLocator())
+    # plt.savefig(os.path.join(output_path, '%d.png' % img_i), bbox_inches='tight', pad_inches=0.0)
+    # plt.close()
+    return fig
+    
+
+def detect(
+        kitti_weights='../checkpoints/best_weights_kitti.pth',
+        config_path='../config/yolov3-kitti.cfg',
+        class_path='../data/names.txt',
+        image_path='../data/samples/',
+        output_path='../output',
+):
+    """
+        Script to run inference on sample images. It will store all the inference results in /output directory (
+        relative to repo root)
+        
+        Args
+            kitti_weights: Path of weights
+            config_path: Yolo configuration file path
+            class_path: Path of class names txt file
+            
+    """
+    cuda = torch.cuda.is_available()
+    os.makedirs(output_path, exist_ok=True)
+
+    # Set up model
+    model = Darknet(config_path, img_size=416)
+    model.load_weights(kitti_weights)
+
+    if cuda:
+        model.cuda()
+        print("Cuda available for inference")
+
+    model.eval()  # Set in evaluation mode
+
+    # Allegro.ai dataview
+    dataview = DataView()
+    dataview.add_query(dataset_name='Kitti 2D', version_name='testing')
+    singleframe_list = dataview.to_list()
+
+    # original Dataset, ImageFolder (running over local files)
+    # torch_dataset_object = ImageFolder(image_path, img_size=416)
+
+    # Allegro.ai Torch Dataset (replacing the original ImageFolder)
+    limit_num_test_frame = 10
+    torch_dataset_object = AllegroDataset(singleframe_list[:limit_num_test_frame], train=False)
+
+    # Torch DataLoader
+    dataloader = DataLoader(torch_dataset_object, batch_size=2, shuffle=False, num_workers=0)
+
+    classes = load_classes(class_path)  # Extracts class labels from file
+
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+    imgs = []  # Stores image paths
+    img_detections = []  # Stores detections for each image index
+
+    print('data size : %d' % len(dataloader))
+    print('\nPerforming object detection:')
+    prev_time = time.time()
+    for batch_i, (img_paths, input_imgs, ignored_labels) in enumerate(dataloader):
+        # Configure input
+        input_imgs = Variable(input_imgs.type(Tensor))
+
+        # Get detections
+        with torch.no_grad():
+            detections = model(input_imgs)
+            detections = non_max_suppression(detections, 80, 0.8, 0.4)
+            # print(detections)
+
+        # Log progress
+        current_time = time.time()
+        inference_time = datetime.timedelta(seconds=current_time - prev_time)
+        prev_time = current_time
+        print('\t+ Batch %d, Inference Time: %s' % (batch_i, inference_time))
+
+        # Save image and detections
+        imgs.extend(img_paths)
+        img_detections.extend(detections)
+
+    # Bounding-box colors
+    # cmap = plt.get_cmap('tab20b')
+    #  cmap = plt.get_cmap('tab10')
+    #  colors = [cmap(i) for i in np.linspace(0, 1, 20)]
+
+    print('\nSaving images:')
+    # Iterate through images and save plot of detections
+    for img_i, (path, detections) in enumerate(zip(imgs, img_detections)):
+
+        print("(%d) Image: '%s'" % (img_i, path))
+        # Mark this frame as annotated
+        singleframe_list[img_i].add_annotation(frame_class=['annotated'])
+
+        # Create plot
+        img = np.array(Image.open(path))
+        plt.figure()
+        fig, ax = plt.subplots(1)
+        ax.imshow(img)
+
+        kitti_img_size = 416
+
+        # The amount of padding that was added
+        pad_x = max(img.shape[0] - img.shape[1], 0) * (kitti_img_size / max(img.shape))
+        pad_y = max(img.shape[1] - img.shape[0], 0) * (kitti_img_size / max(img.shape))
+        # Image height and width after padding is removed
+        unpad_h = kitti_img_size - pad_y
+        unpad_w = kitti_img_size - pad_x
+
+        # Draw bounding boxes and labels of detections
+        if detections is not None:
+            print(type(detections))
+            print(detections.size())
+            unique_labels = detections[:, -1].cpu().unique()
+            # n_cls_preds = len(unique_labels)
+            # bbox_colors = random.sample(colors, n_cls_preds)
+            for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
+
+                print('\t+ Label: %s, Conf: %.5f' % (classes[int(cls_pred)], cls_conf.item()))
+                # Rescale coordinates to original dimensions
+                box_h = int(((y2 - y1) / unpad_h) * (img.shape[0]))
+                box_w = int(((x2 - x1) / unpad_w) * (img.shape[1]))
+                y1 = int(((y1 - pad_y // 2) / unpad_h) * (img.shape[0]))
+                x1 = int(((x1 - pad_x // 2) / unpad_w) * (img.shape[1]))
+
+                color = _bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
+                # Create a Rectangle patch
+                bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2,
+                                         edgecolor=color,
+                                         facecolor='none')
+                # Add the bbox to the plot
+                ax.add_patch(bbox)
+                # Add label
+                plt.text(x1, y1 - 30, s=classes[int(cls_pred)] + ' ' + str('%.4f' % cls_conf.item()), color='white',
+                         verticalalignment='top',
+                         bbox={'color': color, 'pad': 0})
+
+                # store back to allegroai platform
+                singleframe_list[img_i].add_annotation(
+                    box2d_xywh=[x1, y1, box_w, box_h],
+                    labels=[classes[int(cls_pred)]],
+                    confidence=cls_conf.item())
+
+        # Save generated image with detections
+        plt.axis('off')
+        plt.gca().xaxis.set_major_locator(NullLocator())
+        plt.gca().yaxis.set_major_locator(NullLocator())
+        plt.savefig(os.path.join(output_path, '%d.png' % img_i), bbox_inches='tight', pad_inches=0.0)
+        plt.close()
+
+    print('Registering back annotations to allegroai platform')
+    # get our version, if we failed, create a new one
+    try:
+        dataset = DatasetVersion.get_version(
+            dataset_name='KITTI 2D', version_name='replace_with_user_name')
+    except ValueError:
+        # let's create a new version, with parent version as testing
+        print('Creating new dataset version')
+        dataset = DatasetVersion.create_version(
+            dataset_name='KITTI 2D', version_name='replace_with_user_name',
+            parent_version_names=['testing'])
+    # now send all the frames we detected
+    dataset.add_frames(singleframe_list[:limit_num_test_frame])
+
+
+if __name__ == '__main__':
+    task = Task.init(project_name='example', task_name='detect with allegroai and register back data')
+    torch.multiprocessing.freeze_support()
+    kwargs = dict(
+        kitti_weights='./checkpoints/best_weights_kitti.pth',
+        config_path='./config/yolov3-kitti.cfg',
+        class_path='./data/names.txt',
+        image_path='./data/samples/',
+        output_path='./output',
+    )
+    detect(**kwargs)
---
+++ src/main.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 16:33:09 2019
+
+@author: user
+"""
+
+from __future__ import division
+
+import csv
+import os
+import warnings
+
+import torch
+from torch.utils.data import DataLoader
+
+from src.dataset import KITTI2D
+from src.dataset_allegro import AllegroDataset
+from src.model import Darknet
+from src.train_model import train_model
+from allegroai import DataView, Task
+from allegroai.dataview import SingleFrame
+
+warnings.filterwarnings("ignore")
+
+
+def main(train_path="../data/train/images/",
+         val_path="../data/train/images/",
+         labels_path="../data/train/yolo_labels/",
+         weights_path="../checkpoints/",
+         preload_weights_file="best_weights_kitti.pth",
+         output_path="../output",
+         yolo_config_file="../config/yolov3-kitti.cfg",
+         fraction=0.8,
+         learning_rate=1e-3,
+         weight_decay=1e-4,
+         batch_size=2,
+         epochs=30,
+         freeze_struct=[True, 5], val_conf_thre=0.8):
+    """
+        This is the point of entry to the neural network program.
+        All the training history will be saved as a csv in the output path
+        
+        Args
+            train_path (string): Directory containing the training images
+            val_path (string):: Directory containing the val images
+            labels_path (string):: Directory containing the yolo format labels for data
+            weights_path (string):: Directory containing the weights (new weights for this program will also be added
+            here)
+            preload_weights_file (string): Name of preload weights file
+            output_path (string): Directory to store the training history outputs as csv
+            yolo_config_file (string): file path of yolo configuration file
+            fraction (float): fraction of data to use for training
+            learning_rate (float): initial learning rate
+            weight_decay (float): weight decay value
+            batch_size (int): batch_size for both training and validation
+            epochs (int): maximum number of epochs to train the model
+            freeze_struct (list): [bool, int] indicating whether to freeze the Darknet backbone and until which epoch
+            should it be frozen
+            
+        Returns
+            None
+    
+    """
+
+    # Load dataview
+    dataview = DataView()
+    dataview.add_query(
+        dataset_name='Kitty 2D', version_name='training', roi_query=['Car'])
+    dataview.set_labels({
+        'Car': 0, 'Van': 1, 'Truck': 2, 'Pedestrian': 3, 'Person_sitting': 4, 'Cyclist': 5, 'Tram': 6, 'Misc': 7
+    })
+    train_list = dataview.to_list()
+    dataview.prefetch_files()
+
+    dataview = DataView('validation')
+    dataview.add_query(
+        dataset_name='Kitty 2D', version_name='testing')
+    dataview.set_labels({
+        'Car': 0, 'Van': 1, 'Truck': 2, 'Pedestrian': 3, 'Person_sitting': 4, 'Cyclist': 5, 'Tram': 6, 'Misc': 7
+    })
+    validation_list = dataview.to_list()
+    dataview.prefetch_files()
+
+    print("Dataview split: training {} images, validation {} images".format(len(train_list), len(validation_list)))
+
+    # Set up checkpoints path
+    checkpoints_path = weights_path
+
+    # Set up env variables and create required directories
+    os.makedirs(output_path, exist_ok=True)
+    os.makedirs(checkpoints_path, exist_ok=True)
+
+    # Set up cuda
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    print("Available device = ", device)
+
+    # Create model and load pretrained darknet weights
+    model = Darknet(yolo_config_file)
+    print("Loading imagenet weights to darknet")
+    try:
+        model.load_weights(os.path.join(weights_path, preload_weights_file))
+    except FileNotFoundError:
+        print('Skipping loading weights file, {} could not be found'.format(
+            os.path.join(weights_path, preload_weights_file)))
+    model.to(device)
+    # print(model)
+
+    # create torch Datasets
+    train_dataset = AllegroDataset(train_list, train=True)
+    valid_dataset = AllegroDataset(validation_list, train=False)
+
+    # Create dataloaders
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+
+    # Create optimizers
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=learning_rate,
+                                 weight_decay=weight_decay)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10)
+
+    # Create log csv files
+    train_log_file = open(os.path.join(output_path, "train_results.csv"), "w", newline="")
+    valid_log_file = open(os.path.join(output_path, "valid_results.csv"), "w", newline="")
+    train_csv = csv.writer(train_log_file)
+    valid_csv = csv.writer(valid_log_file)
+
+    print("Starting to train yolov3 model...")
+
+    # Train model here
+    train_model(model,
+                device,
+                optimizer,
+                lr_scheduler,
+                train_dataloader,
+                valid_dataloader,
+                train_csv,
+                valid_csv,
+                weights_path,
+                max_epochs=epochs,
+                tensor_type=torch.cuda.FloatTensor,
+                update_gradient_samples=1,
+                freeze_darknet=freeze_struct[0],
+                freeze_epoch=freeze_struct[1],
+                val_conf_thre=val_conf_thre)
+
+    # Close the log files
+    train_log_file.close()
+    valid_log_file.close()
+
+    print("Training completed")
+
+
+if __name__ == "__main__":
+    task = Task.init('examples', 'hyper-dataset training yolo kitty', output_uri=True)
+    # set default container, so it is easier to replicate (not a must)
+    task.set_base_docker(docker_cmd="python:3.8-bullseye", docker_setup_bash_script=["apt install -y libgl1-mesa-glx"])
+    # connect arguments so that we could later control them from the UI
+    kwargs = dict(
+        train_path="./data/train/images/",
+        val_path="./data/train/images/",
+        labels_path="./data/train/yolo_labels/",
+        weights_path="./checkpoints/",
+        preload_weights_file="best_weights_kitti.pth",
+        output_path="./output",
+        yolo_config_file="./config/yolov3-kitti.cfg",
+        fraction=0.8,
+        learning_rate=1e-3,
+        weight_decay=1e-4,
+        batch_size=2,
+        epochs=1,
+        freeze_struct=[False, 0],
+        val_conf_thre=0.5)
+    task.connect(kwargs)
+    # get started
+    main(**kwargs)
+
